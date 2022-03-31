11406d230a738e2424cf17b74a81c59ac7dd02d6
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2022-03-31 14:59:05 +0300

    Give the waveforms a more rounded sound

--- hewmp_backend/text2music/static/text2music/js/waveform.js
+++ hewmp_backend/text2music/static/text2music/js/waveform.js
@@ -3,42 +3,42 @@
 function createWaveforms(audioCtx) {
   const result = {
     warm1: audioCtx.createPeriodicWave(
-      new Float32Array([0, 1, 0.2, 0.2, 0.2, 0.1, 0.1, 0.05]),
-      new Float32Array([0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.00])
+      new Float32Array([0, 10, 2, 2, 2, 1, 1, 0.5, 0.1]),
+      new Float32Array([0,  0, 0, 0, 0, 0, 0, 0.0, 0.0])
     ),
     warm2: audioCtx.createPeriodicWave(
-      new Float32Array([0, 1, 0.5, 0.333, 0.2, 0.1]),
-      new Float32Array([0, 0, 0.0, 0.000, 0.0, 0.0])
+      new Float32Array([0, 10, 5, 3.33, 2, 1, 0.2, 0.1]),
+      new Float32Array([0,  0, 0, 0.00, 0, 0, 0.0, 0.0])
     ),
     warm3: audioCtx.createPeriodicWave(
-      new Float32Array([0, 1, 0.5, 0.5, 0.3]),
-      new Float32Array([0, 0, 0.0, 0.0, 0.0])
+      new Float32Array([0, 10, 5, 5, 3, 0.2, 0.1]),
+      new Float32Array([0,  0, 0, 0, 0, 0.0, 0.0])
     ),
     warm4: audioCtx.createPeriodicWave(
-      new Float32Array([0, 1, 0.2, 0.2, 0.1]),
-      new Float32Array([0, 0, 0.0, 0.0, 0.0])
+      new Float32Array([0, 10, 2, 2, 1, 0.1]),
+      new Float32Array([0,  0, 0, 0, 0, 0.0])
     ),
     octaver: audioCtx.createPeriodicWave(
-      new Float32Array([0,1,0.5,0,0.333,0,0,0,0.25,0,0,0,0,0,0,0,0.166]),
-      new Float32Array([0,0,0,  0,0,    0,0,0,0,   0,0,0,0,0,0,0,0])
+      new Float32Array([0,100,50,0,33,0,0,0,25,0,0,0,0,0,0,0,16]),
+      new Float32Array([0,  0, 0,0, 0,0,0,0, 0,0,0,0,0,0,0,0, 0])
     ),
     brightness: audioCtx.createPeriodicWave(
-      new Float32Array([0,1,0,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.1,0.1,0.1,0.1,0.1]),
-      new Float32Array([0,0,0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+      new Float32Array([0,10,0,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,.75,.5,.2,.1]),
+      new Float32Array([0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,.00,.0,.0,.0])
     ),
     harmonicbell: audioCtx.createPeriodicWave(
-      new Float32Array([0, 1, 0.2, 0.2, 0.2, 0.2,0,0,0,0,0,0.7]),
-      new Float32Array([0, 0, 0.0, 0.0, 0.0, 0.0,0,0,0,0,0,0])
+      new Float32Array([0, 10, 2, 2, 2, 2,0,0,0,0,0,7]),
+      new Float32Array([0,  0, 0, 0, 0, 0,0,0,0,0,0,0])
     ),
     semisine: audioCtx.createPeriodicWave(
       new Float32Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
       new Float32Array([0, 1, 0.25, 0.111111, 0.0625, 0.04, 0.027777, 0.020408, 0.015625, 0.0123456, 0.01, 0.008264, 0.0069444, 0.0059171, 0.005102041, 0.0044444, 0.00390625])
     ),
-    template: audioCtx.createPeriodicWave(
-      // first element is DC offset, second element is fundamental, third element is 2nd harmonic, etc.
-      new Float32Array([0, 1, 0.5, 0.333, 0.25, 0.2, 0.167]), // sine components
-      new Float32Array([0, 0, 0.0, 0.000, 0.00, 0.0, 0.000])  // cosine components
-    )
+    // template: audioCtx.createPeriodicWave(
+    //   // first element is DC offset, second element is fundamental, third element is 2nd harmonic, etc.
+    //   new Float32Array([0, 1, 0.5, 0.333, 0.25, 0.2, 0.167]), // sine components
+    //   new Float32Array([0, 0, 0.0, 0.000, 0.00, 0.0, 0.000])  // cosine components
+    // )
   };
 
   // Elliptic Theta 3
