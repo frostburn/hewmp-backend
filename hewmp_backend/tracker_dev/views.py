7282c041cb8cf592ffc4041b7bc3c21cdd14edf5
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2022-04-09 20:11:20 +0300

    Update Lumi Tracker Vue distribution

--- hewmp_backend/tracker_dev/views.py
+++ hewmp_backend/tracker_dev/views.py
@@ -8,8 +8,8 @@ def index(request):
     <link rel="icon" href="/static/tracker-dev/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Lumi Tracker</title>
-    <script type="module" crossorigin src="/static/tracker-dev/assets/index.4d4d18ee.js"></script>
-    <link rel="stylesheet" href="/static/tracker-dev/assets/index.99e30aec.css">
+    <script type="module" crossorigin src="/static/tracker-dev/assets/index.6bf1b9f6.js"></script>
+    <link rel="stylesheet" href="/static/tracker-dev/assets/index.9e1d75b2.css">
   </head>
   <body>
     <div id="app"></div>
