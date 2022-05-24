2257bef29bd8bec4a10c7595e152dfb2f8564e1e
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2022-05-24 19:47:52 +0300

    Update temperament game dist with subgroups and warts

--- hewmp_backend/temperament_game/views.py
+++ hewmp_backend/temperament_game/views.py
@@ -8,13 +8,14 @@ def index(request):
     <link rel="icon" href="/static/temperament-game/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Temperament Game</title>
-    <script type="module" crossorigin src="/static/temperament-game/assets/index.5c691c32.js"></script>
-    <link rel="stylesheet" href="/static/temperament-game/assets/index.9d10c201.css">
+    <script type="module" crossorigin src="/static/temperament-game/assets/index.b651966d.js"></script>
+    <link rel="stylesheet" href="/static/temperament-game/assets/index.1b8e282b.css">
   </head>
   <body>
     <div id="app"></div>
     
   </body>
 </html>
+
 """
     return HttpResponse(content)
