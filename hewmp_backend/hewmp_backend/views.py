f3d97d8b8537e17b1e0a591763b41056cf0c5e00
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2026-09-07 13:26:57 +0300

    Update home page links

--- hewmp_backend/hewmp_backend/views.py
+++ hewmp_backend/hewmp_backend/views.py
@@ -5,6 +5,13 @@ def index(request):
     <html>
     <head><title>Lumi Pakkanen | Music, Theory & Free Software</title></head>
     <body>
+        <h2>Projects</h2>
+        <a href="https://scaleworkshop.plainsound.org/">Scale Workshop 3</a>
+        <a href="https://xenpaper.lumipakkanen.com/">Xenpaper 2</a>
+        <a href="https://xenpaper3.lumipakkanen.com/">Xenpaper 3</a>
+        <h2>Arhives</h2>
+        <a href="https://jarzombek.lumipakkanen.com/">Ralph Jarzombek - Microtonal music archive</a>
+        <h2>Dead projects</h2>
         <a href="/tracker/">LumiTracker</a><br>
         <a href="/temperament-game/">Temperament "Game"</a><br>
         <a href="/text2music/">Text2Music</a><br>
